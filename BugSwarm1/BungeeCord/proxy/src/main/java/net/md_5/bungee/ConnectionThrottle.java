f6d5229de9edcff2f47241a843bebaa9e5c2a138
xyz538 <xaybzc0804@gmail.com>
2026-04-15 14:41:42 +0530

    benchmark added

---
+++ BugSwarm1/BungeeCord/proxy/src/main/java/net/md_5/bungee/ConnectionThrottle.java
@@ -0,0 +1,30 @@
+package net.md_5.bungee;
+
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+import java.net.InetAddress;
+import java.util.concurrent.TimeUnit;
+
+public class ConnectionThrottle
+{
+
+    private final Cache<InetAddress, Boolean> throttle;
+
+    public ConnectionThrottle(int throttleTime)
+    {
+        this.throttle = CacheBuilder.newBuilder()
+                .concurrencyLevel( Runtime.getRuntime().availableProcessors() )
+                .initialCapacity( 100 )
+                .expireAfterWrite( throttleTime, TimeUnit.MILLISECONDS )
+                .build();
+    }
+
+    public boolean throttle(InetAddress address)
+    {
+        Boolean isThrottledBoolean = throttle.getIfPresent( address );
+        boolean isThrottled = isThrottledBoolean;
+        throttle.put( address, true );
+
+        return isThrottled;
+    }
+}
